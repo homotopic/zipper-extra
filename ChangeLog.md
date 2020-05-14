89f9fa8b289f02c6a01a74a7b2a309c4c9c59e07
Daniel Firth <locallycompact@gmail.com>
2020-05-14 13:11:59 +0000

    Add MonadThrow version of paginate

--- ChangeLog.md
+++ ChangeLog.md
@@ -1,5 +1,9 @@
 # Changelog for zipper-extra
 
+## v0.1.1.0
+
+Add paginate' and exception handling for pagination failure.
+
 ## v0.1.0.0
 
 Reexport `Control.Comonad.Store.Zipper` with some utility functions.
--- src/Control/Comonad/Zipper/Extra.hs
+++ src/Control/Comonad/Zipper/Extra.hs
@@ -5,18 +5,39 @@ module Control.Comonad.Zipper.Extra (
 , Control.Comonad.Store.Zipper.unzipper
 , Control.Comonad.Store.Zipper.size
 , paginate
+, paginate'
+, PaginationException(..)
 , zipperNextMaybe
 , zipperPreviousMaybe
 , zipperWithin
 ) where
 
+import Control.Monad.Catch
+import Control.Exception
 import Control.Comonad.Store
 import Control.Comonad.Store.Zipper
 import Data.List.Split
+import Data.Typeable
 
 -- | Turn a list into a zipper of chunks of length n
 paginate :: Int -> [a] -> Maybe (Zipper [] [a])
 paginate n = zipper . chunksOf n
+
+data PaginationException = EmptyContentsError | ZeroPageSize | UnknownPaginationException
+  deriving (Show, Eq, Typeable)
+
+instance Exception PaginationException where
+  displayException EmptyContentsError         = "Can not create a Zipper of length zero."
+  displayException ZeroPageSize               = "Can not divide into pages of size zero."
+  displayException UnknownPaginationException = "Unknown pagination exception."
+
+-- | Like `paginate`, but throw an exception if it can't create the zipper.
+paginate' :: MonadThrow m => Int -> [a] -> m (Zipper [] [a])
+paginate' n xs = case paginate n xs of
+                    Just x -> return x
+                    Nothing -> if n == 0 then throwM ZeroPageSize
+                               else if length xs == 0 then throwM EmptyContentsError
+                               else throwM UnknownPaginationException
 
 -- | Return the peek of the next element if it exists.
 zipperNextMaybe :: Zipper t a -> Maybe a
@@ -29,4 +50,3 @@ zipperPreviousMaybe xs = if pos xs > 0 then Just (peeks (+ (-1)) xs) else Nothin
 -- Return a list of elements within 'r' hops either side of the zipper target.
 zipperWithin :: Int -> Zipper t a -> [a]
 zipperWithin r xs = (`peek` xs) <$>  [(max 0 (pos xs - r)) .. (min (size xs -1) (pos xs + r))]
-
